f260349a915df878c9a8018da49a9a5d5ceca4b3
Bernardo <programming@bernardomg.com>
2020-07-30 01:07:35 +0200

    The details are read from the API

--- src/app/charclass.service.ts
+++ src/app/charclass.service.ts
@@ -3,7 +3,7 @@ import { Charclass } from './charclass';
 import { ApiResponse } from './api-response';
 import { CHARCLASSES } from './mock-charclasses';
 import { Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { catchError, map, tap } from 'rxjs/operators';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
@@ -12,7 +12,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class CharclassService {
 
-  private heroesUrl = 'https://www.dnd5eapi.co/api/classes';  // URL to web api
+  private charclassesUrl = 'https://www.dnd5eapi.co/api/classes';  // URL to web api
 
   constructor(
     private http: HttpClient,
@@ -20,15 +20,46 @@ export class CharclassService {
   ) { }
 
   getCharClasses(): Observable<Charclass[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<ApiResponse<Charclass[]>>(this.heroesUrl).pipe(map((response: ApiResponse<Charclass[]>) => {return response.results}));
+    return this.http.get<ApiResponse<Charclass[]>>(this.charclassesUrl).pipe(
+      map((response: ApiResponse<Charclass[]>) => { return response.results }),
+      catchError(this.handleError<Charclass[]>('getCharClasses', []))
+    ).pipe(
+      tap(_ => this.log('fetched classes')),
+      catchError(this.handleError<Charclass[]>('getCharClasses', []))
+    );
   }
 
   getCharClass(id: string): Observable<Charclass> {
-    // TODO: send the message _after_ fetching the hero
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(CHARCLASSES.find(charclass => charclass.index === id));
+    const url = `${this.charclassesUrl}/${id}`;
+    return this.http.get<Charclass>(url).pipe(
+      tap(_ => this.log(`fetched class index=${id}`)),
+      catchError(this.handleError<Charclass>('getCharClass'))
+    );
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`CharclassService: ${message}`);
+  }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
