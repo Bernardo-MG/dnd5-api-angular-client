d8ea69dde3fec2aa0c136d36c83622d0b691bd3e
Bernardo <programming@bernardomg.com>
2020-07-29 19:22:18 +0200

    Extended based on the tutorial

--- src/app/charclass/charclass.component.ts
+++ src/app/charclass/charclass.component.ts
@@ -11,9 +11,15 @@ export class CharclassComponent implements OnInit {
 
   charclasses = CHARCLASSES;
 
+  selectedCharClass: Charclass;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSelect(charclass: Charclass): void {
+    this.selectedCharClass = charclass;
   }
 
 }
