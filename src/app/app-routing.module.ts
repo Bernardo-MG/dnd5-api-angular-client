7f55191b472e1f1fc8e51ad06c5db2da24c20d89
Bernardo <programming@bernardomg.com>
2020-07-29 22:52:09 +0200

    Added route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CharclassComponent } from './charclass/charclass.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'classes', component: CharclassComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
