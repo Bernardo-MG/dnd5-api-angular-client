2381e4c866b714debd5dcdc603a2161bfb12f81e
Bernardo <programming@bernardomg.com>
2021-07-26 23:16:07 +0200

    Renamed components

--- src/app/charclass-view/charclass-list/charclass-list.component.ts
+++ src/app/charclass-view/charclass-list/charclass-list.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { Charclass } from '@app/models/charclass';
 
 @Component({
-  selector: 'app-charclass-list',
+  selector: 'charclass-list',
   templateUrl: './charclass-list.component.html',
   styleUrls: ['./charclass-list.component.sass']
 })
