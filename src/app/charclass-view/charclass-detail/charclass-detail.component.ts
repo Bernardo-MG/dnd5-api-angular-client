b29472906823d871cd0093308c4b8051d9ed3142
Bernardo <bmg@envira.es>
2021-07-26 09:08:31 +0200

    Moved data initialization to main component

--- src/app/charclass-view/charclass-detail/charclass-detail.component.ts
+++ src/app/charclass-view/charclass-detail/charclass-detail.component.ts
@@ -1,34 +1,16 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Charclass } from '../../models/charclass';
-import { ActivatedRoute } from '@angular/router';
-import { CharclassService } from '../../services/charclass.service';
 
 @Component({
   selector: 'app-charclass-detail',
   templateUrl: './charclass-detail.component.html',
   styleUrls: ['./charclass-detail.component.sass']
 })
-export class CharclassDetailComponent implements OnInit {
+export class CharclassDetailComponent {
 
   @Input() charclass!: Charclass;
 
-  constructor(
-    private route: ActivatedRoute,
-    private charclassService: CharclassService
-  ) { }
-
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.getCharClass(params['id']);
-    });
-  }
-
-  getCharClass(id: string): void {
-    if (id) {
-      this.charclassService.getCharClass(id)
-        .subscribe(charclass => this.charclass = charclass);
-    }
-  }
+  constructor() { }
 
   asList(data: any[]) {
     return data.map(obj => obj.name);
--- src/app/charclass-view/charclass/charclass.component.ts
+++ src/app/charclass-view/charclass/charclass.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Charclass } from '../../models/charclass';
 import { CharclassService } from '../../services/charclass.service';
 
@@ -10,12 +11,29 @@ import { CharclassService } from '../../services/charclass.service';
 export class CharclassComponent implements OnInit {
 
   charclasses: Charclass[] = [];
+  
+  charclass!: Charclass;
 
-  constructor(private charclassService: CharclassService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private charclassService: CharclassService
+  ) { }
 
   ngOnInit(): void {
     // Loads character classes
     this.charclassService.getCharClasses().subscribe(charclasses => this.charclasses = charclasses);
+
+    // Loads selected character class
+    this.route.params.subscribe(params => {
+      this.getCharClass(params['id']);
+    });
+  }
+
+  getCharClass(id: string): void {
+    if (id) {
+      this.charclassService.getCharClass(id)
+        .subscribe(charclass => this.charclass = charclass);
+    }
   }
 
 }
