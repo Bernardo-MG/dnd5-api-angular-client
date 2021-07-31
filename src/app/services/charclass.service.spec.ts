5264bbd3870452cee085add681b070d44ee08ee4
Bernardo <programming@bernardomg.com>
2021-08-01 00:04:02 +0200

    Added test

--- src/app/services/charclass.service.spec.ts
+++ src/app/services/charclass.service.spec.ts
@@ -1,20 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { CharclassService } from './charclass.service';
+import { of } from 'rxjs';
+import { Charclass } from '@app/models/charclass';
 
 describe('CharclassService', () => {
   let service: CharclassService;
+  let httpClientSpy: { get: jasmine.Spy };
 
   beforeEach(() => {
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
     TestBed.configureTestingModule({
-      imports: [HttpClientModule]
+      imports: [HttpClientModule],
+      providers: [
+        { provide: HttpClient, useValue: httpClientSpy }
+      ]
     });
     service = TestBed.inject(CharclassService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('returns value from request', () => {
+    const expectedData: Charclass[] =
+      [{ index: '1', name: 'A', hit_die: 1, proficiency_choices: [], proficiencies: [], saving_throws: [], starting_equipment: '', class_levels: '', subclasses: [], url: '' }];
+
+    httpClientSpy.get.and.returnValue(of(expectedData));
+
+    service.getCharClasses();
+
+    expect(httpClientSpy.get).toHaveBeenCalled();
   });
+
 });
