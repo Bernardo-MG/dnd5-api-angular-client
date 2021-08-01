3fccee44cd80ed129b46280e482b5da72d3a0fcd
Bernardo <programming@bernardomg.com>
2021-08-01 13:19:38 +0200

    Mocked service

--- src/app/views/charclass-view/charclass-view.component.spec.ts
+++ src/app/views/charclass-view/charclass-view.component.spec.ts
@@ -1,22 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-
 import { CharclassViewComponent } from './charclass-view.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CharclassModule } from '@app/charclass/charclass.module';
+import { CharclassService } from '@app/services/charclass.service';
+import { of } from 'rxjs';
+import { Charclass } from '@app/models/charclass';
 
 describe('CharclassViewComponent', () => {
   let component: CharclassViewComponent;
   let fixture: ComponentFixture<CharclassViewComponent>;
+  let serviceSpy: { getCharClasses: jasmine.Spy };
 
   beforeEach(async () => {
+    const expectedData: Charclass[] =
+      [{ index: '1', name: 'A', hit_die: 1, proficiency_choices: [], proficiencies: [], saving_throws: [], starting_equipment: '', class_levels: '', subclasses: [], url: '' }];
+
+    serviceSpy = jasmine.createSpyObj('CharclassService', ['getCharClasses']);
+    serviceSpy.getCharClasses.and.returnValue(of(expectedData));
+
     await TestBed.configureTestingModule({
       declarations: [CharclassViewComponent],
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule,
         CharclassModule
+      ],
+      providers: [
+        { provide: CharclassService, useValue: serviceSpy }
       ]
     })
       .compileComponents();
