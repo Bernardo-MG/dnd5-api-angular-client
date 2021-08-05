041184776de9a861957759233ac3bef0d722884e
Bernardo <bmg@envira.es>
2021-08-05 11:14:40 +0200

    Corrected imports

--- src/app/charclass/charclass.module.ts
+++ src/app/charclass/charclass.module.ts
@@ -5,7 +5,6 @@ import { MatListModule } from '@angular/material/list';
 
 import { CharclassListComponent } from './charclass-list/charclass-list.component';
 import { CharclassDetailComponent } from './charclass-detail/charclass-detail.component';
-import { RouterTestingModule } from '@angular/router/testing';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -15,7 +14,6 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterTestingModule,
     RouterModule,
     MatListModule
   ],
