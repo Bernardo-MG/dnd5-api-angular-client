ba0185528d632916a96270ce38dfbc7588eefbad
Bernardo-MG <programming@bernardo-mg.com>
2023-03-04 13:33:28 +0100

    Using plain Bootstrap 5

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
 
-import { LayoutModule } from '@angular/cdk/layout';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
@@ -18,13 +12,7 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     NoopAnimationsModule,
-    LayoutModule,
     RouterModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    MatSidenavModule,
-    MatToolbarModule
   ],
   exports: [
     NavigationMenuComponent
