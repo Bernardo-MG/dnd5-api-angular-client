78cf233f4a8fa2cb1c4607927bcf3330f548eee8
Bernardo-MG <programming@bernardo-mg.com>
2023-03-14 23:12:52 +0100

    Cleaned up code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeViewComponent } from './layout/views/home-view/home-view.component';
 
-const layoutModule = () => import('@app/layout/layout.module').then(x => x.LayoutModule);
 const charsModule = () => import('@app/dnd5/charclass/charclass.module').then(x => x.CharclassModule);
 
 const routes: Routes = [
