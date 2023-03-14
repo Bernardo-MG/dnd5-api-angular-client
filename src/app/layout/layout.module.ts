df6aa813ea87633385cb0bd415ed062d26e5305d
Bernardo-MG <programming@bernardo-mg.com>
2023-03-14 23:16:06 +0100

    Removed redundant component

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LayoutComponent } from './components/layout/layout.component';
 import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    LayoutComponent,
     DropdownMenuComponent
   ],
   imports: [
@@ -18,7 +16,7 @@ import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.
     RouterModule,
   ],
   exports: [
-    LayoutComponent
+    NavbarComponent
   ]
 })
 export class LayoutModule { }
