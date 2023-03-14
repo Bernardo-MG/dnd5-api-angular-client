671ee5e381b4ac80ee44135b0241911cc7c683d2
Bernardo-MG <programming@bernardo-mg.com>
2023-03-14 23:32:04 +0100

    Added top padding

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,12 +2,14 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    LayoutComponent,
     DropdownMenuComponent
   ],
   imports: [
@@ -16,7 +18,7 @@ import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.
     RouterModule,
   ],
   exports: [
-    NavbarComponent
+    LayoutComponent
   ]
 })
 export class LayoutModule { }
