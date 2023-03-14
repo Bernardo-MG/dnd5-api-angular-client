43758322fa4292d3f1bf649ab0a2af89db2f21e9
Bernardo-MG <programming@bernardo-mg.com>
2023-03-14 23:09:48 +0100

    Corrected import

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { LayoutComponent } from './layout/layout/layout.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { DropdownMenuComponent } from './components/dropdown-menu/dropdown-menu.component';
 
 @NgModule({
