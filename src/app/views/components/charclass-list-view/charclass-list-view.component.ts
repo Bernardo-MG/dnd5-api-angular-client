2e9a2c0eb0b50b0650b83b509d53e703fb175ecf
Bernardo-MG <programming@bernardo-mg.com>
2023-03-16 00:07:40 +0100

    Removed unused import

--- src/app/views/components/charclass-list-view/charclass-list-view.component.ts
+++ src/app/views/components/charclass-list-view/charclass-list-view.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CharclassService } from '@app/dnd5/charclass/services/charclass.service';
 import { Reference } from '@app/dnd5/models/info/reference';
 import { MenuLink } from '@app/layout/models/menu-link';
-import { link } from 'fs';
 
 @Component({
   selector: 'app-charclass-list-view',
