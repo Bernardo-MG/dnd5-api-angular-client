0ef89a3c976626ec7bf74344df3e484157e766ce
Bernardo-MG <programming@bernardo-mg.com>
2023-03-20 16:04:24 +0100

    Pagination buttons are disabled correctly

--- src/app/components/pagination-template/pagination-template.component.spec.ts
+++ src/app/components/pagination-template/pagination-template.component.spec.ts
@@ -20,4 +20,54 @@ describe('PaginationTemplateComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('by default only the backward and forward buttons exist', () => {
+    const buttons = fixture.nativeElement.querySelectorAll('button');
+    expect(buttons.length).toEqual(2);
+  });
+
+  // Disable based on status flags
+
+  it('should disable the backward button by default', () => {
+    const button = fixture.nativeElement.querySelectorAll('button')[0];
+    expect(button.disabled).toEqual(true);
+  });
+
+  it('should disable the forward button by default', () => {
+    const button = fixture.nativeElement.querySelectorAll('button')[1];
+    expect(button.disabled).toEqual(true);
+  });
+
+  it('should disable the backward button when it is the first page', () => {
+    component.first = true;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button')[0];
+    expect(button.disabled).toEqual(true);
+  });
+
+  it('should enable the backward button when it is not the first page', () => {
+    component.first = false;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button')[0];
+    expect(button.disabled).toEqual(false);
+  });
+
+  it('should disable the forward button when it is the last page', () => {
+    component.last = true;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button')[1];
+    expect(button.disabled).toEqual(true);
+  });
+
+  it('should enable the forward button when it is not the last page', () => {
+    component.last = false;
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button')[1];
+    expect(button.disabled).toEqual(false);
+  });
+
 });
