aa5e92c5fb59ee84b88594c46acf625bf1c902ff
Bernardo-MG <programming@bernardomg.com>
2023-03-21 10:17:24 +0100

    Added structure tests

--- src/app/components/pagination-template/pagination-template.component.spec.ts
+++ src/app/components/pagination-template/pagination-template.component.spec.ts
@@ -21,10 +21,90 @@ describe('PaginationTemplateComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  // List structure
+
+  it('by default only the backward and forward list items exist', () => {
+    const items = fixture.nativeElement.querySelectorAll('li');
+    expect(items.length).toEqual(2);
+  });
+
+  it('should add a list item when receiving a page for the first half', () => {
+    component.firstHalf = [1];
+    fixture.detectChanges();
+
+    const items = fixture.nativeElement.querySelectorAll('li');
+    expect(items.length).toEqual(3);
+  });
+
+  it('should add list items, including separator, when receiving a page for the second half and there is a first half', () => {
+    component.firstHalf = [1];
+    component.secondHalf = [5];
+    fixture.detectChanges();
+
+    const items = fixture.nativeElement.querySelectorAll('li');
+    expect(items.length).toEqual(5);
+  });
+
+  it('should add no list items when receiving a page for the second half when there is no first half', () => {
+    component.secondHalf = [5];
+    fixture.detectChanges();
+
+    const items = fixture.nativeElement.querySelectorAll('li');
+    expect(items.length).toEqual(2);
+  });
+
+  // Movement buttons
+
   it('by default only the backward and forward buttons exist', () => {
     const buttons = fixture.nativeElement.querySelectorAll('button');
     expect(buttons.length).toEqual(2);
   });
+
+  // Page buttons
+
+  it('should add a button when receiving a page for the first half', () => {
+    component.firstHalf = [1];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toEqual(3);
+  });
+
+  it('should add multiple buttons when receiving multiple pages for the first half', () => {
+    component.firstHalf = [1, 2, 3];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toEqual(5);
+  });
+
+  it('should add no button when receiving a page for the second half when there is no first half', () => {
+    component.secondHalf = [5];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toEqual(2);
+  });
+
+  it('should add a button when receiving a page for the second half and there is a first half', () => {
+    component.firstHalf = [1];
+    component.secondHalf = [5];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toEqual(4);
+  });
+
+  it('should add multiple buttons when receiving multiple pages for both halves', () => {
+    component.firstHalf = [1, 2, 3];
+    component.secondHalf = [5, 6, 7];
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelectorAll('button');
+    expect(button.length).toEqual(8);
+  });
+
+  // List structure
 
   // Disable based on status flags
 
@@ -110,55 +190,6 @@ describe('PaginationTemplateComponent', () => {
 
     const button = fixture.nativeElement.querySelectorAll('button')[1];
     expect(button.disabled).toEqual(false);
-  });
-
-  // Page buttons
-
-  it('should create only two buttons by default (backward and forward)', () => {
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(2);
-  });
-
-  it('should add a button when receiving a page for the first half', () => {
-    component.firstHalf = [1];
-    fixture.detectChanges();
-
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(3);
-  });
-
-  it('should add multiple buttons when receiving multiple pages for the first half', () => {
-    component.firstHalf = [1, 2, 3];
-    fixture.detectChanges();
-
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(5);
-  });
-
-  it('should add no button when receiving a page for the second half when there is no first half', () => {
-    component.secondHalf = [5];
-    fixture.detectChanges();
-
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(2);
-  });
-
-  it('should add a button when receiving a page for the second half and there is a first half', () => {
-    component.firstHalf = [1];
-    component.secondHalf = [5];
-    fixture.detectChanges();
-
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(4);
-  });
-
-  it('should add multiple buttons when receiving multiple pages for both halves', () => {
-    component.firstHalf = [1, 2, 3];
-    component.secondHalf = [5, 6, 7];
-    fixture.detectChanges();
-
-    const button = fixture.nativeElement.querySelectorAll('button');
-    expect(button.length).toEqual(8);
   });
 
 });
