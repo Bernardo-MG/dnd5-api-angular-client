a00fac69cfa7b8ec253bd9734b3dd345389248a6
Bernardo-MG <programming@bernardo-mg.com>
2023-03-22 01:09:39 +0100

    Prepares flags once

--- src/app/components/pagination/pagination-template/pagination-template.component.ts
+++ src/app/components/pagination/pagination-template/pagination-template.component.ts
@@ -1,11 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'dnd5-pagination-template',
   templateUrl: './pagination-template.component.html',
   styleUrls: ['./pagination-template.component.sass']
 })
-export class PaginationTemplateComponent {
+export class PaginationTemplateComponent implements OnChanges {
 
   @Input() public disabled = false;
 
@@ -23,6 +23,22 @@ export class PaginationTemplateComponent {
 
   @Output() public goTo = new EventEmitter<number>();
 
+  public backwardDisabled = false;
+
+  public forwardDisabled = false;
+
+  public drawCenter = false;
+
+  public drawRight = false;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.backwardDisabled = ((this.first) || (this.disabled));
+    this.forwardDisabled = ((this.last) || (this.disabled));
+
+    this.drawCenter = ((this.left.length > 0) && (this.center.length > 0));
+    this.drawRight = ((this.left.length > 0) && (this.right.length > 0));
+  }
+
   public onBackward() {
     this.goTo.emit(this.page - 1);
   }
@@ -36,33 +52,33 @@ export class PaginationTemplateComponent {
   }
 
   public isBackwardDisabled(): boolean {
-    return (this.first) || (this.disabled);
+    return this.backwardDisabled;
   }
 
   public isForwardDisabled(): boolean {
-    return (this.last) || (this.disabled);
+    return this.forwardDisabled;
   }
 
   public isGoToDisabled(): boolean {
-    return (this.disabled);
+    return this.disabled;
   }
 
   public isAbleToDrawCenterRange() {
-    return (this.left.length > 0) && (this.center.length > 0);
+    return this.drawCenter;
   }
 
   public isAbleToDrawRightRange() {
-    return (this.left.length > 0) && (this.right.length > 0);
+    return this.drawRight;
   }
 
-  public isCurrent(page: number){
+  public isCurrent(page: number) {
     return page === this.page;
   }
 
-  public getCurrentLabel(page: number){
+  public getCurrentLabel(page: number) {
     let label;
 
-    if(page === this.page) {
+    if (page === this.page) {
       // Current page
       label = 'page';
     } else {
