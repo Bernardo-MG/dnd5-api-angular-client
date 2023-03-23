fb8b6a5beb87a1d4a79e4c95bf1da9ccaab6ec59
Bernardo-MG <programming@bernardo-mg.com>
2023-03-23 11:53:08 +0100

    Cleaned up code

--- src/app/components/pagination/pagination-template/pagination-template.component.ts
+++ src/app/components/pagination/pagination-template/pagination-template.component.ts
@@ -12,54 +12,103 @@ export class PaginationTemplateComponent {
    */
   @Input() public disabled = false;
 
+  /**
+   * Disabled backward flag. To disable the backward button.
+   */
   @Input() public disableBackward = true;
 
+  /**
+   * Disabled forward flag. To disable the forward button.
+   */
   @Input() public disableForward = true;
 
-  @Input() public page = 0;
+  /**
+   * Current page.
+   */
+  @Input() public current = 0;
 
+  /**
+   * Left page range.
+   */
   @Input() public left: number[] = [];
 
+  /**
+   * Center page range.
+   */
   @Input() public center: number[] = [];
 
+  /**
+   * Right page range.
+   */
   @Input() public right: number[] = [];
 
   @Output() public goTo = new EventEmitter<number>();
 
+  /**
+   * Sends a "go to page" event for the previous page.
+   */
   public onBackward() {
-    this.goTo.emit(this.page - 1);
+    this.goTo.emit(this.current - 1);
   }
 
+  /**
+   * Sends a "go to page" event for the next page.
+   */
   public onForward() {
-    this.goTo.emit(this.page + 1);
+    this.goTo.emit(this.current + 1);
   }
 
+  /**
+   * Sends a "go to page" event for selected page.
+   */
   public onGoTo(page: number) {
     this.goTo.emit(page);
   }
 
+  /**
+   * Indicates if the backward button should be disabled.
+   * 
+   * @returns true if the backward button should be disabled, false otherwise
+   */
   public isBackwardDisabled(): boolean {
     return ((this.disableBackward) || (this.disabled));
   }
 
+  /**
+   * Indicates if the forward button should be disabled.
+   * 
+   * @returns true if the forward button should be disabled, false otherwise
+   */
   public isForwardDisabled(): boolean {
     return ((this.disableForward) || (this.disabled));
   }
 
-  public isGoToDisabled(): boolean {
-    return this.disabled;
-  }
-
+  /**
+   * Indicates if the center range should be drawn. This depends on both the left and center range having values.
+   * 
+   * @returns true if the center range should be drawn, false otherwise
+   */
   public isAbleToDrawCenterRange() {
     return ((this.left.length > 0) && (this.center.length > 0));
   }
 
+  /**
+   * Indicates if the right range should be drawn. This depends on both the left and right range having values.
+   * 
+   * @returns true if the right range should be drawn, false otherwise
+   */
   public isAbleToDrawRightRange() {
     return ((this.left.length > 0) && (this.right.length > 0));
   }
 
+  /**
+   * Indicates if the received page is the current page.
+   * 
+   * @param page page to check
+   * @returns true if the page matches the current page, false otherwise
+   */
   public isCurrent(page: number) {
-    return page === this.page;
+    return page === this.current;
   }
 
 }
