7c21be55a440385ab190e2ce065e455fe1abd789
Bernardo-MG <programming@bernardo-mg.com>
2023-03-23 23:13:00 +0100

    Updated tests

--- src/app/components/pagination/pagination/pagination.component.spec.ts
+++ src/app/components/pagination/pagination/pagination.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ButtonComponent } from '@app/components/button/button.component';
 import { BackwardIconComponent } from '@app/components/icons/backward-icon/backward-icon.component';
 import { ForwardIconComponent } from '@app/components/icons/forward-icon/forward-icon.component';
+import { IconsModule } from '@app/components/icons/icons.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { PaginationTemplateComponent } from '../pagination-template/pagination-template.component';
 import { PaginationComponent } from './pagination.component';
@@ -13,7 +14,8 @@ describe('PaginationComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        FontAwesomeModule
+        FontAwesomeModule,
+        IconsModule
       ],
       declarations: [
         PaginationComponent,
