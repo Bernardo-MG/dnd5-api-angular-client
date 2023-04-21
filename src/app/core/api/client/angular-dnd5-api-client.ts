60ad99442b3fab81b9bd28888cca02b99b6328ee
Bernardo-MG <programming@bernardo-mg.com>
2023-04-22 01:58:21 +0200

    Taking API URL from environment

--- src/app/core/api/client/angular-dnd5-api-client.ts
+++ src/app/core/api/client/angular-dnd5-api-client.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ReadOperations } from "@app/core/api/client/read-operations";
+import { environment } from "environments/environment";
 import { AngularReadOperations } from "./angular-read-operations";
 import { Dnd5ApiClient } from "./dnd5-api-client";
 import { CharacterClassQuery } from "./query/charclass/character-class-query";
@@ -11,7 +12,7 @@ import { ProficiencyQuery } from "./query/charclass/proficiency-query";
 })
 export class AngularDnd5ApiClient implements Dnd5ApiClient {
 
-  private rootUrl = 'http://www.dnd5eapi.co/api';
+  private rootUrl = environment.apiUrl;
 
   constructor(
     private http: HttpClient
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://www.dnd5eapi.co/api/classes'
+  apiUrl: 'http://www.dnd5eapi.co/api'
 };
