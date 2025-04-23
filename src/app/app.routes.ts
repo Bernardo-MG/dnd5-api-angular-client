956fb3ce6b1f5ca66e47f84af07f81e942f45a96
Bernardo Martínez Garrido <bernardo.martinez_garrido@contractors.roche.com>
2025-04-23 13:49:21 +0200

    Preparing layout structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { AppHomeComponent } from './home/components/app.home/app.home.component';
+import { AppLayoutComponent } from './layout/components/app.layout/app.layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppLayoutComponent,
+        children: [
+            { path: '', component: AppHomeComponent }
+        ]
+    }
+];
