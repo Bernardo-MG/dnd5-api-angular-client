5ddd1278a69b4f09759267e37b8f6ef899a9130c
Bernardo Martínez Garrido <bernardo.martinez_garrido@contractors.roche.com>
2025-04-23 17:29:26 +0200

    Added side menu

--- src/app/layout/components/app.layout/app.layout.component.ts
+++ src/app/layout/components/app.layout/app.layout.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppSidebarComponent } from '../app.sidebar/app.sidebar.component';
 import { AppTopbarComponent } from '../app.topbar/app.topbar.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, AppTopbarComponent,],
+  imports: [RouterOutlet, AppTopbarComponent, AppSidebarComponent],
   templateUrl: './app.layout.component.html',
   styleUrl: './app.layout.component.scss'
 })
---
+++ src/app/layout/components/app.sidebar/app.sidebar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppMenuComponent } from '../app.menu/app.menu.component';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [AppMenuComponent],
+  templateUrl: './app.sidebar.component.html',
+  styleUrl: './app.sidebar.component.scss'
+})
+export class AppSidebarComponent {
+
+}
