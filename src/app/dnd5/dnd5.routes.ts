2c16f6048b323ac6cecc31d1035738ad97d717e9
Bernardo-MG <programming@bernardomg.com>
2025-04-27 00:38:24 +0200

    Select class

--- src/app/dnd5/dnd5.routes.ts
+++ src/app/dnd5/dnd5.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 import { CharclassListComponent } from './containers/charclass-list/charclass-list.component';
 
 export default [
-  { path: 'classes', component: CharclassListComponent }
+  { path: 'classes', component: CharclassListComponent },
+  { path: 'classes/:id', component: CharclassListComponent }
 ] as Routes;
--- src/app/shared/components/link-list/link-list.component.ts
+++ src/app/shared/components/link-list/link-list.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Link } from '@app/shared/model/link';
 import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-link-list',
-  imports: [TableModule],
+  imports: [CommonModule, RouterLink, TableModule],
   templateUrl: './link-list.component.html'
 })
 export class LinkListComponent {
