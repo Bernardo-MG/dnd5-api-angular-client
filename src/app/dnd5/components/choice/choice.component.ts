b698cd8f0f9a7945f172d5ee3eb1f0920c48080a
Bernardo-MG <programming@bernardomg.com>
2025-05-09 00:57:47 +0200

    Reduced choice

--- src/app/dnd5/components/choice/choice.component.ts
+++ src/app/dnd5/components/choice/choice.component.ts
@@ -1,14 +1,74 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Choice } from '@app/dnd5/models/options/choice';
-import { OptionSetComponent } from '../option-set/option-set.component';
 
 @Component({
   selector: 'dnd5-choice',
-  imports: [OptionSetComponent],
+  standalone: true,
+  imports: [CommonModule, /* recursive import */],
   templateUrl: './choice.component.html'
 })
 export class ChoiceComponent {
+  @Input() option: any;
+  @Input() choice?: Choice;
 
-  @Input() public choice: Choice = new Choice();
+  isChoice(): boolean {
+    return !!(this.option?.choice ?? this.choice);
+  }
 
+  isOptionArray(): boolean {
+    return this.option?.option_set_type === 'options_array';
+  }
+
+  isEquipment(): boolean {
+    return this.option?.option_set_type === 'equipment_category';
+  }
+
+  isResourceList(): boolean {
+    return this.option?.option_set_type === 'resource_list';
+  }
+
+  isReference(): boolean {
+    return this.option?.option_type === 'reference';
+  }
+
+  isCounted(): boolean {
+    return this.option?.option_type === 'counted_reference';
+  }
+
+  isMultiple(): boolean {
+    return this.option?.option_type === 'multiple';
+  }
+
+  getChoice(): Choice {
+    return this.option?.choice ?? this.choice!;
+  }
+
+  getOptions() {
+    return this.option?.options ?? [];
+  }
+
+  getItems() {
+    return this.option?.items ?? [];
+  }
+
+  getItemName() {
+    return this.option?.item?.name ?? '';
+  }
+
+  getCount() {
+    return this.option?.count ?? 0;
+  }
+
+  getOfName() {
+    return this.option?.of?.name ?? '';
+  }
+
+  getEquipmentName() {
+    return this.option?.equipment_category?.name ?? '';
+  }
+
+  getResourceList() {
+    return this.option?.resource_list ?? '';
+  }
 }
