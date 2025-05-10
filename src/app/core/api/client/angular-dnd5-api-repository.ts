dcb6660bc086062ab5788c0a8003478afe3de36b
Bernardo-MG <programming@bernardomg.com>
2025-05-11 01:08:14 +0200

    Removed interface

--- src/app/core/api/client/angular-dnd5-api-repository.ts
+++ src/app/core/api/client/angular-dnd5-api-repository.ts
@@ -2,14 +2,13 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { environment } from "environments/environment";
 import { AngularReadClient } from "./angular-read-client";
-import { Dnd5Repository } from "./dnd5-api-repository";
 import { CharacterClassQuery } from "./query/charclass/character-class-query";
 import { ProficiencyQuery } from "./query/charclass/proficiency-query";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AngularDnd5ApiRepository implements Dnd5Repository {
+export class AngularDnd5ApiRepository {
 
   private readonly client;
 
