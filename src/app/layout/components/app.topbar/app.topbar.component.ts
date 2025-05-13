1ece9463ee99084b13ee35ed7878f43adad157b5
Bernardo-MG <programming@bernardomg.com>
2025-05-14 00:16:05 +0200

    Title in topbar

--- src/app/layout/components/app.topbar/app.topbar.component.ts
+++ src/app/layout/components/app.topbar/app.topbar.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { StyleClassModule } from 'primeng/styleclass';
 import { LayoutService } from '../../services/layout.service';
 
 @Component({
   selector: 'app-topbar',
-  imports: [CommonModule, StyleClassModule],
+  imports: [CommonModule, RouterLink, StyleClassModule],
   templateUrl: './app.topbar.component.html'
 })
 export class AppTopbarComponent {
